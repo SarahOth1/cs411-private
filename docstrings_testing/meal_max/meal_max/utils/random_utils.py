9686de0898a2c24be651f54e4864debab460e98d
SarahOth1 <144280948+SarahOth1@users.noreply.github.com>
2024-11-06 16:37:27 -0500

    Update random_utils.py

--- docstrings_testing/meal_max/meal_max/utils/random_utils.py
+++ docstrings_testing/meal_max/meal_max/utils/random_utils.py
@@ -1,38 +1,97 @@
-import logging
-import requests
-
-from meal_max.utils.logger import configure_logger
-
-logger = logging.getLogger(__name__)
-configure_logger(logger)
+import pytest
 
 
-def get_random() -> float:
-    url = "https://www.random.org/decimal-fractions/?num=1&dec=2&col=1&format=plain&rnd=new"
+from models.battle_model import BattleModel
+from models.kitchen_model import Meal
 
-    try:
-        # Log the request to random.org
-        logger.info("Fetching random number from %s", url)
 
-        response = requests.get(url, timeout=5)
+@pytest.fixture()
+def battle_model():
+   """Fixture to provide a new instance of BattleModel for each test."""
+   return BattleModel()
 
-        # Check if the request was successful
-        response.raise_for_status()
 
-        random_number_str = response.text.strip()
+@pytest.fixture
+def mock_update_meal_stats(mocker):
+   """Mock the update_meal_stats function for testing purposes."""
+   return mocker.patch("models.battle_model.update_meal_stats")
 
-        try:
-            random_number = float(random_number_str)
-        except ValueError:
-            raise ValueError("Invalid response from random.org: %s" % random_number_str)
 
-        logger.info("Received random number: %.3f", random_number)
-        return random_number
+"""Fixtures providing sample meals for the tests."""
+@pytest.fixture
+def sample_meal1():
+   return Meal(1, 'Tikka Masala', 'Indian', 25.0, 'MED')
 
-    except requests.exceptions.Timeout:
-        logger.error("Request to random.org timed out.")
-        raise RuntimeError("Request to random.org timed out.")
 
-    except requests.exceptions.RequestException as e:
-        logger.error("Request to random.org failed: %s", e)
-        raise RuntimeError("Request to random.org failed: %s" % e)
+@pytest.fixture
+def sample_meal2():
+   return Meal(2, 'Pasta', 'Italian', 20.0, 'LOW')
+
+
+@pytest.fixture
+def sample_battle(sample_meal1, sample_meal2):
+   return [sample_meal1, sample_meal2]
+
+
+
+
+#####################
+   # Test Battle Management Functions
+#####################
+def test_battle_error(battle_model, sample_meal1, caplog):
+   """Test the VlaueError of battle between if combatants are < 2."""
+   battle_model.prep_combatant(sample_meal1)
+   battle_model.battle()
+   with pytest.raises(ValueError, match="Two combatants must be prepped for a battle."):
+       battle_model.battle()
+  
+
+
+def test_battle(battle_model, sample_meal1, sample_meal2):
+   """Test the winner of battle between first 2 combatants."""
+   battle_model.prep_combatant(sample_meal1)
+   battle_model.prep_combatant(sample_meal2)
+   battle_model.battle()
+   winnerMeal = battle_model.battle()
+   assert winnerMeal == sample_meal1.meal
+# there is no way to tell because it depends on a random number
+  
+
+
+def test_clear_combatants(battle_model, sample_meal1):
+   """Test clearing the entire combatants list."""
+   battle_model.prep_combatant(sample_meal1)
+   battle_model.clear_combatants()
+   assert len(battle_model.ombatants) == 0, "Playlist should be empty after clearing"
+
+
+def test_get_combatants(battle_model, sample_meal1, sample_meal2):
+   """Test successfully retrieving all combatants."""
+   battle_model.prep_combatant(sample_meal1)
+   battle_model.prep_combatant(sample_meal2)
+   all_coms = battle_model.get_combatants()
+   assert len(all_coms) == 2
+   assert all_coms[0].id == 1
+   assert all_coms[1].id == 2
+
+
+def test_prep_combatant(battle_model, sample_meal1):
+   """Test adding a combatant to the combatants list."""
+   battle_model.prep_combatant(sample_meal1)
+   assert len(battle_model.combatants) == 1
+   assert battle_model.combatants[0].meal == 'Tikka Masala'
+
+
+def test_prep_combatant_error(battle_model, sample_meal1, sample_meal2):
+   """Test failing to add a combatant to the full combatants list."""
+   battle_model.prep_combatant(sample_meal1)
+   battle_model.prep_combatant(sample_meal2)
+   with pytest.raises(ValueError, match="Combatant list is full, cannot add more combatants."):
+       battle_model.prep_combatant(sample_meal2)
+
+
+def test_get_battle_score(battle_model, sample_meal1):
+   """Test getting the battle score."""
+   difficulty_modifier = {"HIGH": 1, "MED": 2, "LOW": 3}
+   score = battle_model.get_battle_score(battle_model, sample_meal1)
+   assert score == (sample_meal1.price * len(sample_meal1.cuisine)) - difficulty_modifier[sample_meal1.difficulty]
